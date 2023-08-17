c93b9aa23e3b58f37c57ed3c1adf608addc65856
Akashay Prakash Singh <112270350+AkashayP@users.noreply.github.com>
2023-08-18 00:53:37 +0530

    Create first.dart

---
+++ first.dart
@@ -0,0 +1,30 @@
+void main(){
+  print('hello world');
+  print(3+2-2*2/4);
+  print('3'+'2');
+  String name = 'Ramesh';
+  print (name);
+  double percent = 75.00;
+    print ( percent );
+  dynamic somevalue = 15;
+  print (somevalue.isEven);
+  int num = 13;
+  print (num);
+ // assgning the value again
+  num = num+17;
+  print (num);
+  // var/ const/ final
+  var value1 = '45';
+  const value2 = '43';
+  final value3 = '44';
+  
+  print (value1);
+  print (value2);
+  print (value3);
+  
+  value1 = '55';
+  value2 = '56'; // cant assign new value
+  value3 = '53';
+  
+  
+}
