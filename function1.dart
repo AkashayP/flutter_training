4ffcd29e3f1f05e88179b0917e695155652149c1
Akashay Prakash Singh <112270350+AkashayP@users.noreply.github.com>
2023-08-28 17:30:10 +0530

    function1.dart

--- function1.dart
+++ function1.dart
@@ -3,8 +3,17 @@ void main(){
   sum(30, 20);
   sum(33,66);
   sum(66,33);
+  print (subtract(12, 2));
+  print (subtract(12, 4));
+  print (subtract(12, 6));
+  
 }
+
 
 void sum(int num1 , int num2){
  print (num1+ num2) ;
+}
+
+int subtract (int x , int y ){
+  return x-y;  
 }
